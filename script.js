d458bee4314ba71b08fff87165186d047a9accc9
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-08-10 15:23:49 +0200

    short

--- script.js
+++ script.js
@@ -1,96 +1,129 @@
-// JavaScript for URL Shortening App
+//WOW js
+new WOW().init();
 
-document.addEventListener('DOMContentLoaded', () => {
-  const urlForm = document.getElementById('url-form');
-  const linkInput = document.getElementById('link');
-  const errorMessage = document.getElementById('error-message');
-  const shortenedLinksList = document.getElementById('shortened-links');
-  const shortUrlContainer = document.getElementById('shortened-url');
-  const shortUrlText = document.getElementById('short-url');
-  const copyShortUrlButton = document.getElementById('copy-short-url');
+$(document).ready(function () {
+    //Smooth Scroll to Div
+    $(".smooth-scroll").on('click', function (event) {
+        event.preventDefault();
+        var hash = '#header';
+        var size = $("#header").height() / 2;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top + size
+        }, 400, function () {
+            window.location.hash = hash;
+        });
+    });
+})
 
-  urlForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+//Loader
+$(window).on('load', function () {
+    $("#loader").addClass("d-none");
+})
 
-    const url = linkInput.value.trim();
-
-    if (url === '') {
-      errorMessage.textContent = 'Please enter a URL.';
-      return;
-    }
-
-    try {
-      const response = await fetch(`https://api.tinyurl.com/dev/api?url=${encodeURIComponent(url)}`);
-      const data = await response.json();
-
-      if (response.ok) {
-        const shortUrl = data.tiny_url;
-        shortUrlText.textContent = shortUrl;
-        shortUrlContainer.style.display = 'block';
-      } else {
-        errorMessage.textContent = 'Error shortening the URL.';
-      }
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-
-    const shortenedUrl = shortenUrl(url);
-    addShortenedLink(url, shortenedUrl);
-
-    linkInput.value = '';
-    errorMessage.textContent = '';
-  });
-
-  copyShortUrlButton.addEventListener('click', () => {
-    const shortUrl = shortUrlText.textContent;
-    copyToClipboard(shortUrl);
-  });
-
-  loadShortenedLinks();
+//Change image on hover
+$(".alt").hide();
+$(".socmed").mouseover(function () {
+    $(this).children(".main").hide();
+    $(this).children(".alt").show();
+}).mouseleave(function () {
+    $(this).children(".main").show();
+    $(this).children(".alt").hide();
 });
 
-function shortenUrl(url) {
-  // Replace this with your actual URL shortening logic
-  return 'shortened-link';
+//Check if valid URL
+var theurl = $("#theurl");
+var linkcont = $("#item");
+//Get existing links
+getLinks();
+
+//Remove error message on change
+$("#theurl").change(function () {
+    $("#theurl").removeClass('err');
+});
+
+$("#form-shorten").submit(function (e) {
+    e.preventDefault();
+    $("#theurl").removeClass('err');
+    if (URLIsValid(theurl.val())) {
+        $(".ripple").css("display", "block");
+        $("#theurl").prop('disabled', true);
+        //Post to API
+        fetch("https://api.shrtco.de/v2/shorten?url=" + theurl.val())
+            .then((res) => res.json())
+            .then((data) => {
+                storeLink(theurl.val(), data.result.short_link);
+                // $(".ripple").css("display", "none");
+                // $(this).prop('disabled', false);
+            });
+    } else {
+        $("#theurl").addClass('err');
+        if (theurl.val() == "") {
+            $("#err-msg").text('Please add a link.');
+        } else {
+            $("#err-msg").text('Please enter a valid URL.');
+        }
+    }
+});
+
+var temp = $("<input>");
+$(".close").click(function (event) {
+    event.preventDefault();
+    $(".copy").removeClass("copied").text("Copy");
+    temp.remove();
+    //Delete an item
+    var linkkey = $(this).data("linkkey");
+    items = JSON.parse(sessionStorage.getItem("items"));
+    items.splice(linkkey, 1);
+    sessionStorage.setItem("items", JSON.stringify(items));
+    // getLinks();
+    location.reload();
+});
+
+$(".copy").click(function (e) {
+    e.preventDefault();
+    $(".copy").removeClass("copied").text("Copy");
+    //Copy shortlink
+    var copyText = $(this).data("copylink");
+    $("body").append(temp);
+    temp.val(copyText).select();
+    document.execCommand("copy");
+    temp.remove();
+    //Copied! text
+    $(this).addClass("copied").text("Copied!");
+});
+
+
+function URLIsValid(theurl) {
+    return /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.test(theurl)
 }
 
-function addShortenedLink(originalUrl, shortenedUrl) {
-  const shortenedLinksList = document.getElementById('shortened-links');
-  const li = document.createElement('li');
-  li.innerHTML = `
-    <p><strong>Original URL:</strong> ${originalUrl}</p>
-    <p><strong>Shortened URL:</strong> ${shortenedUrl}</p>
-    <button class="copy-button" data-link="${shortenedUrl}">Copy</button>
-  `;
-
-  shortenedLinksList.appendChild(li);
-
-  const savedLinks = JSON.parse(localStorage.getItem('shortenedLinks')) || [];
-  savedLinks.push({ originalUrl, shortenedUrl });
-  localStorage.setItem('shortenedLinks', JSON.stringify(savedLinks));
+function storeLink(link, shortlink) {
+    let item = { link, shortlink }; //data to be stored
+    let items = get(); // get existing links
+    items.push(item); // add new links to existing links
+    sessionStorage.setItem("items", JSON.stringify(items)); // store locally
+    // getLinks();
+    location.reload();
 }
 
-function copyToClipboard(text) {
-  navigator.clipboard.writeText(text)
-    .then(() => {
-      alert('Link copied to clipboard!');
+function get() {
+    //Get items in local storage
+    let items;
+    if (sessionStorage.getItem("items") == null) {
+        items = [];
+    } else {
+        items = JSON.parse(sessionStorage.getItem("items"));
+    }
+    return items;
+}
+
+function getLinks() {
+    linkcont.html("");
+    let items = get(); // get existing links
+    // for each item run function showShortLinks
+    var x = 0;
+    items.forEach(i => {
+        linkcont.append('<div class="item wow animate__animated animate__fadeIn"><div class="text"><p class="close" data-linkkey="' + x + '">x</p><span class="link">' + i.link + '</span><span class="result">' + i.shortlink + '<button class="copy button big" data-copylink="' + i.shortlink + '">Copy</button></span></div></div>');
+        x++;
     })
-    .catch((error) => {
-      console.error('Failed to copy text: ', error);
-    });
-}
-
-function loadShortenedLinks() {
-  const savedLinks = JSON.parse(localStorage.getItem('shortenedLinks')) || [];
-  const shortenedLinksList = document.getElementById('shortened-links');
-
-  savedLinks.forEach((link) => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-      <p><strong>Original URL:</strong> ${link.originalUrl}</p>
-      <p><strong>Shortened URL:</strong> ${link.shortenedUrl}</p>
-      <button class="copy-button" data-link="${link.shortenedUrl}">Copy</button>
-    `;
-    shortenedLinksList.appendChild(li);
-  });
 }
